c2ee09cf09135c97a736b2259e069693ecf5e2d2
wangtanli <2772609079@qq.com>
2020-04-04 21:55:23 +0800

    working tree clean

--- get_requests.py
+++ get_requests.py
@@ -1,6 +1,17 @@
 import requests
+from bs4 import  BeautifulSoup
 
 url="http://www.cntour.cn"
 response=requests.get(url)
 
-print(response.text)
+ # print(response.text)
+
+soup=BeautifulSoup(response.text,'lxml')
+data=soup.select("#main > div > div.mtop.firstMod.clearfix > div.leftBox > div:nth-child(3) > ul > li:nth-child(2) > a")
+print(data)
+for item in data
+      result={
+          'title':item.get_text(),
+          'link':itme.get('herf')
+      }
+      print(result)
