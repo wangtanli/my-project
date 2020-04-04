24e99201d15d3045e696e25363b63e0916030f62
wangtanli <2772609079@qq.com>
2020-04-04 22:08:37 +0800

    finished get cntour

--- get_requests.py
+++ get_requests.py
@@ -4,14 +4,14 @@ from bs4 import  BeautifulSoup
 url="http://www.cntour.cn"
 response=requests.get(url)
 
- # print(response.text)
-
+# print(response.text)
 soup=BeautifulSoup(response.text,'lxml')
 data=soup.select("#main > div > div.mtop.firstMod.clearfix > div.leftBox > div:nth-child(3) > ul > li:nth-child(2) > a")
 print(data)
-for item in data
-      result={
-          'title':item.get_text(),
-          'link':itme.get('herf')
-      }
-      print(result)
+for item in data:
+    result={
+      'title':item.get_text(),
+      'link':item.get('herf')
+    }
+print(result)
+
