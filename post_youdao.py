5cfde48a384bb34cf6255399efd651cef69882de
wangtanli <2772609079@qq.com>
2020-04-05 16:29:53 +0800

    作业3 11121904729王昙莉

--- post_youdao.py
+++ post_youdao.py
@@ -1,15 +1,32 @@
 import requests
 
 url="http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
+
+
+def get_salt():
+    return '15860628873889'
+
+
+def get_sign():
+    return '16bfeef8bfe1bab0f14a688e05092703'
+
+
+def get_ts():
+    import time
+    t = time.time()
+    ts = str(int(round(t * 1000)))
+    return ts
+      #'1586068687933'
+
 form_data={
     'i': '我和你都是中国',
     'from': 'zh-CHS',
     'to': 'en',
     'smartresult': 'dict',
     'client': 'fanyideskweb',
-    'salt': '15860628873889',
-    'sign': '16bfeef8bfe1bab0f14a688e05092703',
-    'ts': '1586062887388',
+    'salt': get_salt(),
+    'sign': get_sign(),
+    'ts': get_ts(),
     'bv': 'a9c3483a52d7863608142cc3f302a0ba',
     'doctype': 'json',
     'version': '2.1',
---
+++ test_post_youdao.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest import mock
+
+from post_youdao import *
+
+class PostyoudaoTest(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)
+    def test_get_ts(self):
+        #import time
+        #t= time.time()
+        #ts=str(int(round(t * 1000)))
+        #print(ts)
+        get_ts=mock.Mock(return_value='1586062887388')
+        self.assertEqual('1586062887388',get_ts())
+
+
+if __name__ == '__main__':
+    unittest.main()
